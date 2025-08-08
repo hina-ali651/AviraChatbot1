a3daf8f6c5106c2da7671f78510d0ea654384491
Moiz Ali <moizali2577@gmail.com>
2025-08-08 17:26:19 +0500

    Update fast.py with latest changes

--- hello_agent/fast.py
+++ hello_agent/fast.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://chatbot1-2mikcqkx7-hina-alis-projects-de38f00f.vercel.app"
+        "https://chatbot1-red-iota.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
